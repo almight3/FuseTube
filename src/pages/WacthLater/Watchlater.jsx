366467109630de117d53c767c37562f4a823b332
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-11-04 10:58:40 +0530

    watchlater page

--- src/pages/WacthLater/Watchlater.jsx
+++ src/pages/WacthLater/Watchlater.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import {useEffect} from 'react';
+import  {useSelector,useDispatch} from "react-redux";
+import {getUserWatchlater,removeWatchLater} from "../../store/watchLaterSlice";
+import { Ring } from '@uiball/loaders';
+import AdvanceCard from '../../component/Card/AdvanceCard/AdvanceCard';
 
 function Watchlater() {
+  const {status,watchLater} = useSelector(state=>state.watchLater);
+  const dispatch = useDispatch();
+  
+  useEffect(()=>{
+  dispatch(getUserWatchlater())
+  },[dispatch])
+
   return (
-    <div>Watchlater</div>
+    <>
+    {
+      status ==="loading"  ?
+      <div className='m-auto absolute inset-x-56	mt-80 w-28'>
+        <Ring 
+          size={80}
+          lineWeight={3}
+          speed={2} 
+          color="#26C281" 
+        />
+      </div> :
+      <div className='flex flex-col box-border w-7/12	relative top-28 left-80 '>
+            <h2 className='text-xl text-white mx-8 font-semibold p-2'>Liked</h2>
+      
+         {watchLater?.map((video)=>{
+         return <AdvanceCard video={video} dispatchFunc={removeWatchLater} />
+      })}
+     </div>
+    }
+  </>
   )
 }
 
