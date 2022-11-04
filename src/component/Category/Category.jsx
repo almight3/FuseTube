8d4f69d00d58384f951e66b0d17bd7a490de2027
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-11-04 11:22:06 +0530

    feat - added filter by category

--- src/component/Category/Category.jsx
+++ src/component/Category/Category.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import "./Category.css"
-const categories = ["All","Gaming","Freestyle Rap","Hip Hope","Anime","Podcast" ]
-function Category() {
+const categories = ["All","Gaming","Freestyle Rap","Anime","Podcast" ]
+function Category({setCategory}) {
   return (
     <div className='flex flex-row flex-wrap justify-center box-border w-10/12 ml-72 p-3 mb-6 bg-slate-800 opacity-95	 category-border sticky top-20 z-10'>
         {
-            categories.map((category)=><button className='text-white bg-slate-900 mx-5 list-none  px-3 py-1 rounded-full'>{category}</button>)
+            categories.map((category)=><button className='text-white bg-slate-900 mx-5 list-none  px-3 py-1 rounded-full' value={category} onClick={(e)=>setCategory(e.target.value)}>{category}</button>)
         }
     </div>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,21 +3,30 @@ import Card from '../../component/Card/Card';
 import Category from '../../component/Category/Category';
 import {useDispatch,useSelector} from "react-redux";
 import {getAllVideos} from "../../store/videoSlice";
-import { Ring } from '@uiball/loaders'
+import { Ring } from '@uiball/loaders';
 
+const filterByCatgory = (data,category)=>{
+  if(category=="All"){
+    return data
+  }
+  let filter = data.filter((video)=>video.category === category)
+  return filter
+}
 
 function Home() {
   const dispatch = useDispatch();
   const {data,error,status} = useSelector((state)=>state.video);
+  const [category,setCategory] = useState("All");
   useEffect(()=>{
   dispatch(getAllVideos())
   },[dispatch])
   
+  console.log(category)
   if(status==="error")
   return <h1 className='mx-auto my-10 text-white'>Error....</h1>
   return (
     <>
-    <Category />
+    <Category setCategory={setCategory} />
     {status ==="loading" ?  
     <div className='m-auto	mt-80 w-28'>
     <Ring 
@@ -27,8 +36,8 @@ function Home() {
       color="#26C281" 
     />
     </div>:
-    <div className='flex flex-wrap justify-around box-border w-10/12 ml-72 relative top-14'>
-      {data.map((item)=>{
+    <div className='flex flex-wrap justify-around mx-auto box-border w-10/12 ml-72 relative top-14'>
+      {filterByCatgory(data,category).map((item)=>{
         return <Card item={item} />
       })}       
     </div>
